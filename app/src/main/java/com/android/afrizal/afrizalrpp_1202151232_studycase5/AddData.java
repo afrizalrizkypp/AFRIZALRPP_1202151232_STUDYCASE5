a56047fe9058d98e3effbe03c9a50078f5a0edba
afrizalrizkypp <afrizalrizkypp@gmail.com>
2018-03-25 20:43:21 +0700

    push pertama kali

---
+++ app/src/main/java/com/android/afrizal/afrizalrpp_1202151232_studycase5/AddData.java
@@ -0,0 +1,45 @@
+package com.android.afrizal.afrizalrpp_1202151232_studycase5;
+
+/**
+ * Created by Afrizal Rizky on 25/03/2018.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddData {
+    //deklarasi variable
+    String todo, desc, prior;
+
+    //konstruktor
+    public AddData(String todo, String desc, String prior){
+        this.todo=todo;
+        this.desc=desc;
+        this.prior=prior;
+    }
+
+    //method setter dan getter untuk to do , description dan priority
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPrior() {
+        return prior;
+    }
+
+    public void setPrior(String prior) {
+        this.prior = prior;
+    }
+}
